e053199f43e36c7f4a1eeeeb851f6023af7c53e7
SmritiDas <dassmrit@gmail.com>
2019-11-06 18:27:23 -0500

    Add recyclerview for saved recipes

--- app/src/main/java/com/example/android/recipes/adapters/SavedRecipeAdapter.java
+++ app/src/main/java/com/example/android/recipes/adapters/SavedRecipeAdapter.java
@@ -1,28 +1,48 @@
 package com.example.android.recipes.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.android.recipes.R;
+import com.example.android.recipes.models.network.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedRecipeAdapter extends RecyclerView.Adapter<SavedRecipeAdapter.ViewHolder> {
 
     private static final String TAG = "SavedRecipeAdapter";
+    //create arraylists?
+
+    private List<Recipe> savedRecipe = new ArrayList<>();
+    private Context context;
+
+    public SavedRecipeAdapter(Context context){ // figure out constructor
+
+    }
+
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item_rv, parent, false);
+        SavedRecipeAdapter.ViewHolder viewHolder = new SavedRecipeAdapter.ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull SavedRecipeAdapter.ViewHolder holder, int position) {
+        Recipe recipe = savedRecipe.get(position);
+        //What goes here?
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return savedRecipe.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
